03dc6a5b157dd691c4c79a79df4f2a1f077a8424
Danilo Cruz <danilo@plimtelecom.com.br>
2019-05-13 20:34:58 -0300

    .

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 	apt update
 	apt install -y --no-install-recommends sudo net-tools curl nmap tcpdump htop atop mtr vlan ethtool apt-transport-https ca-certificates mc bmon vlan ifenslave-2.6
 	apt install -y --no-install-recommends psmisc git git-core make cmake zlib1g-dev liblua5.1-dev libpcre3-dev build-essential libssl-dev libsnmp-dev linux-headers-`uname -r`
-	apt install -y --no-install-recommends dh-autoreconf libexpat1-dev telnet ntpdate ipset unzip sqlite3 libsqlite3-dev
+	apt install -y --no-install-recommends dh-autoreconf libexpat1-dev telnet ntpdate ipset unzip sqlite3 libsqlite3-dev snmpd
 
 
 #Instalando o FRR
